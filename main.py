377f250782f441a059e13f3b12c6c37f454a4a69
Saet1a <joaorogeroliveira@gmail.com>
2025-10-04 00:28:32 -0300

    Feat: Implement FPS limit and delta time

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import pygame
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -12,8 +14,11 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
         screen.fill("black")
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
