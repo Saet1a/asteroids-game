8a58384c7ee5fc2a6466837af56b72fa9a1e880f
Saet1a <joaorogeroliveira@gmail.com>
2025-10-05 18:54:51 -0300

    Feat: Implement asteroid splitting upon being shot

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ def main():
 
             for shot in shots:
                 if shot.check_collision(asteroid):
-                    asteroid.kill()
+                    asteroid.split()
                     shot.kill()
                     
 
